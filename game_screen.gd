6f37c79f54e3860196543cabb68ebad077ee80d8
Yohairra China Ignacio <151002392+Yohairra@users.noreply.github.com>
2025-02-21 14:04:03 +0800

    add cart and ingredients panel

--- game_screen.gd
+++ game_screen.gd
@@ -1,10 +1,11 @@
 extends Node2D
 @onready var Background = $gameBackground
+@onready var animation_player = $Cart/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Background.play("bg_animation")
-
+	animation_player.play("cartAnimation")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- title_screen.gd
+++ title_screen.gd
@@ -1,17 +1,22 @@
 extends Node2D
 @onready var Background = $titleBackground
 @onready var animation_player = $Panel/AnimationPlayer
-@onready var panel = $Panel 
+@onready var panel = $Panel
+@onready var open_close_btn = $Panel/OpenClose_Btn
 var is_panel_open = false
 
 func _ready():
 	Background.play("default")
 	panel.position.x = 1216
+	open_close_btn.connect("pressed", _toggle_panel)
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		if is_panel_open:
-			animation_player.play("slide_out_panel")
-		else:
-			animation_player.play("slide_in_panel")
-		is_panel_open = !is_panel_open
+		_toggle_panel()
+
+func _toggle_panel():
+	if is_panel_open:
+		animation_player.play("slide_out_panel")
+	else:
+		animation_player.play("slide_in_panel")
+	is_panel_open = !is_panel_open
