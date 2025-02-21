0a3c8922d2fc8c6f3b9535e93ce15ed7efd13d11
Yohairra China Ignacio <151002392+Yohairra@users.noreply.github.com>
2025-02-21 15:24:36 +0800

    Add logic to ConeBtn and ubeContainer

--- game_screen.gd
+++ game_screen.gd
@@ -1,6 +1,12 @@
 extends Node2D
 @onready var Background = $gameBackground
 @onready var animation_player = $Cart/AnimationPlayer
+@onready var cone_btn = $Panel/ConeBtn
+@onready var ube_container = $Cart/Ube
+@onready var cone = $Cart/Cone
+@onready var ube = $Cart/Ube
+
+var is_cone_visible = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +16,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_cone_btn_pressed() -> void:
+	cone.visible = true
+	is_cone_visible = true
+
+func _on_ube_container_pressed() -> void:
+	if is_cone_visible:
+		ube.visible = true
